7d6994da3db7994b06f343106e05c8a07c534073
Chris Chen <wasi.chris@gmail.com>
2021-02-23 17:13:43 +0800

    .

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -2,7 +2,12 @@
 // API Routes let you create an API endpoint inside a Next.js app.
 // You can do so by creating a function inside the pages/api directory that
 // has the following format:
+import firebase from 'firebase/app'
 
-export default (req, res) => {
-  res.status(200).json({ text: 'Hello' })
+export default async (req, res) => {
+  const db = firebase.database()
+  const eventref = db.ref('users/bNWo1umowMRiSQFyHwdVaHvYRRq1/funds')// oh no
+  const snapshot = await eventref.once('value')// oh no
+  const value = snapshot.val()// oh no
+  res.status(200).json({ text: value })
 }
