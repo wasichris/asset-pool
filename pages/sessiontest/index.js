ef9471795a2dacc59d7b1d03eaf4aaba72ee6494
Chris Chen <wasi.chris@gmail.com>
2021-02-24 13:09:40 +0800

    .

--- pages/sessiontest/index.js
+++ pages/sessiontest/index.js
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-import Layout from '../components/Layout'
+import Layout from '../../components/Layout'
 import { withIronSession } from 'next-iron-session'
 
 function SessionTest (/* { user } */) {
